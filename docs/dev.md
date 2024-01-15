68bc71dd022a8b7df1fb5817089a52ef55850be0
vatoer <vatoer.ckplus@gmail.com>
2024-01-15 19:09:23 +0100

    install nextauth,  prepare database prisma adapters

--- docs/dev.md
+++ docs/dev.md
@@ -16,3 +16,123 @@ install shadcn
 ```sh
 pnpm dlx shadcn-ui@latest init
 ```
+
+## Install NextAuth
+
+```sh
+pnpm add next-auth
+```
+
+add config to `.env`
+
+```env
+# openssl rand -hex 32
+NEXTAUTH_SECRET=1facdd00de499fdc304a8ba5a21275c9bdf8250b4e30c8f4c8c9468facc163ba
+NEXTAUTH_URL=http://localhost:3000
+```
+
+## setup prisma
+
+```sh
+pnpm add -D prisma
+```
+
+- create prisma schema
+
+```sh
+pnpm prisma init 
+```
+
+we will move file `prisma/schema.prisma` to `prisma/db-auth`
+
+```sh
+mkdir prisma/db-auth
+mv prisma/schema.prisma prisma/db-auth/schema.prisma
+```
+
+update .env file to reflect your database (here we use mysql)
+
+```env
+DATABASE_1_URL="mysql://root:password@localhost:3306/dbauth?schema=public"
+```
+
+in `prisma/db-auth/schema.prisma`
+
+```prisma
+datasource db {
+  provider     = "mysql"
+  url      = env("DATABASE_1_URL")
+```
+
+## create db for local authentication and save account
+
+create model Account, Session, User , VerificationToken base on <https://authjs.dev/reference/adapter/prisma>
+
+```prisma
+model Account {
+  id                 String  @id @default(cuid())
+  userId             String
+  type               String
+  provider           String
+  providerAccountId  String
+  refresh_token      String?  @db.Text
+  access_token       String?  @db.Text
+  expires_at         Int?
+  token_type         String?
+  scope              String?
+  id_token           String?  @db.Text
+  session_state      String?
+
+  user User @relation(fields: [userId], references: [id], onDelete: Cascade)
+
+  @@unique([provider, providerAccountId])
+}
+
+model Session {
+  id           String   @id @default(cuid())
+  sessionToken String   @unique
+  userId       String
+  expires      DateTime
+  user         User     @relation(fields: [userId], references: [id], onDelete: Cascade)
+}
+
+model User {
+  id            String    @id @default(cuid())
+  name          String?
+  email         String?   @unique
+  emailVerified DateTime?
+  image         String?
+  accounts      Account[]
+  sessions      Session[]
+}
+
+model VerificationToken {
+  identifier String
+  token      String   @unique
+  expires    DateTime
+
+  @@unique([identifier, token])
+}
+```
+
+migrate your model to database
+
+```sh
+pnpm prisma migrate dev --schema ./prisma/db-auth/schema.prisma
+```
+
+## prepare nextauth provider
+
+- you must have google account to use google credentials service
+  
+  // TODO CREATE doc on google credentials service
+
+go to <https://console.cloud.google.com/>
+
+add your google id and secret to `.env`
+
+```env
+# google secret
+GOOGLE_CLIENT_ID="YOURCLIENTID.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET="YOUR-CLIENTSECRET"
+```
---
+++ lib/auth.ts
@@ -0,0 +1,133 @@
+import { db } from "@/lib/db-auth";
+import { compare, hash } from "bcryptjs";
+import { randomBytes } from "crypto";
+import {
+  GetServerSidePropsContext,
+  NextApiRequest,
+  NextApiResponse,
+} from "next";
+import { getServerSession, type NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: "/signin",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    updateAge: 24 * 60 * 60, // 24 hours
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Sign in",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        const user = await db.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (
+          !user ||
+          !user.password ||
+          !(await compare(credentials.password, user.password))
+        ) {
+          return null;
+        }
+        const randomKey = randomBytes(32).toString("hex");
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          randomKey: randomKey,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      //console.log("sign in", { user, account, profile, email, credentials });
+      if (!user || !account) {
+        throw new Error("Invalid sign in");
+      }
+
+      if (account.provider === "google" && profile) {
+        const { name, email, image } = profile;
+        if (!email || !name) {
+          throw new Error("Google account missing email");
+        }
+
+        const img = image || "no-image.png";
+
+        const user = await db.user.upsert({
+          where: {
+            email: profile.email,
+          },
+          create: {
+            email,
+            name,
+            image: img,
+            updatedAt: new Date(),
+          },
+          update: {
+            name: profile.name,
+          },
+        });
+
+        return true;
+      }
+
+      return true;
+    },
+    session: ({ session, token }) => {
+      //console.log("[AUTH_SESSION]", { session, token });
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          randomKey: token.randomKey,
+        },
+      };
+    },
+    jwt: ({ token, user }) => {
+      if (user) {
+        const u = user as unknown as any;
+        return {
+          ...token,
+          id: u.id,
+          randomKey: u.randomKey,
+        };
+      }
+      return token;
+    },
+  },
+};
+
+export function auth(
+  ...args:
+    | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+    | [NextApiRequest, NextApiResponse]
+    | []
+) {
+  return getServerSession(...args, authOptions);
+}
