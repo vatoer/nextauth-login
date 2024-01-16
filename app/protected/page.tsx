723ee31a7ff98e02c1a8429bb401845e2cf3d0b7
vatoer <vatoer.ckplus@gmail.com>
2024-01-16 14:20:28 +0100

    add signout button

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { UserButton } from "@/components/user-button";
+import { signOut } from "next-auth/react";
+import ButtonSignout from "../(auth)/_components/button-signout";
 
 export default function ProtectedPage() {
   return (
@@ -6,6 +9,9 @@ export default function ProtectedPage() {
       <h1>Protected Page</h1>
       <p>This page is protected.</p>
       <UserButton />
+      <div>
+        <ButtonSignout />
+      </div>
     </div>
   );
 }
